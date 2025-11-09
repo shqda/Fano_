55417e8f4c1cb577b89c3714f20e282986291c1b
Artyom Nikitin <artemf642@gmail.com>
2025-11-09 16:36:05 +0300

    Documentaion reupload

---
+++ docs/dir_09f7776ceddd7774bc1093593209b93d.js
@@ -0,0 +1,8 @@
+var dir_09f7776ceddd7774bc1093593209b93d =
+[
+    [ "cmd_flags.cpp", "dd/d1a/cmd__flags_8cpp.html", "dd/d1a/cmd__flags_8cpp" ],
+    [ "cmd_flags.h", "da/de8/cmd__flags_8h.html", "da/de8/cmd__flags_8h" ],
+    [ "FileCompressor.cpp", "d7/d79/_file_compressor_8cpp.html", null ],
+    [ "FileCompressor.h", "da/dec/_file_compressor_8h.html", "da/dec/_file_compressor_8h" ],
+    [ "main.cpp", "df/d0a/main_8cpp.html", "df/d0a/main_8cpp" ]
+];
---
+++ docs/search/functions_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['decode_5fbitstream_0',['decode_bitstream',['../d2/dbf/class_file_compressor.html#ae070b8a0439417042c0cc6d31acd8597',1,'FileCompressor']]],
+  ['decompress_1',['decompress',['../d2/dbf/class_file_compressor.html#a1831c73242b2a185eadbfc251fbea272',1,'FileCompressor']]],
+  ['do_5ffano_5falgorithm_2',['do_Fano_Algorithm',['../d2/dbf/class_file_compressor.html#ac84f2952172782b0bac572136a9d2178',1,'FileCompressor']]]
+];
